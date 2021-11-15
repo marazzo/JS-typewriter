63da31a8f1e35d635641e32cd76003e87488d83a
Marianne Azzopardi <marianneazzopardi@Mariannes-MacBook-Air.local>
2021-11-15 13:29:12 +0000

    Minor changes

--- typewriter/app.js
+++ typewriter/app.js
@@ -1,13 +1,3 @@
-const otherPhrases = [
-"Don't watch the clock; do what it does. Keep going. - Sam Levenson",
-"If you can dream it, you can do it. - Walt Disney",
-"Ever tried. Ever failed. No matter. Try Again. Fail again. Fail better. - Samuel Beckett",
-"With the new day comes new strength and new thoughts. - Eleanor Roosevelt",
-"Start where you are. Use what you have. Do what you can. - Arthur Ashe",
-"When something is important enough, you do it even if the odds are not in your favor. - Elon Musk",
-]
-
-
 const textDisplay = document.getElementById('text')
 const phrases = [
   "Don't watch the clock; do what it does. Keep going. - Sam Levenson",
@@ -17,7 +7,7 @@ const phrases = [
   "Start where you are. Use what you have. Do what you can. - Arthur Ashe",
   "When something is important enough, you do it even if the odds are not in your favor. - Elon Musk",
   ]
-  
+
 let currentPhrase = []
 let i = 0
 let j = 0
@@ -27,26 +17,19 @@ let isEnd = false
 function loop(){
   textDisplay.innerHTML = currentPhrase.join("")
   isEnd = false
-
   if (i < phrases.length) {
-
     if (!isDeleting && j <= phrases[i].length){
       currentPhrase.push(phrases[i][j])
       j++
-      console.log('add a letter' + phrases[i][j])
     }
-
     if (isDeleting && j <= phrases[i].length ){
       currentPhrase.pop(phrases[i][j])
       j--
-      console.log('remove a letter' + phrases[i][j])
     }
-
     if (j == phrases[i].length){
       isEnd = true
       isDeleting = true
     }
-
     if (isDeleting && j==0){
       currentPhrase = []
       isDeleting = false
@@ -55,14 +38,11 @@ function loop(){
     if (i == phrases.length){
       i = 0
     }
-    
-
   }
   const spedUp = Math.random() * (80 - 50) + 50
   const normalSpeed = Math.random() * (300 - 200) + 200
   const time = isEnd ? 3000 : isDeleting ? spedUp : normalSpeed
   setTimeout(loop, time)
 }
-
 loop()
 
